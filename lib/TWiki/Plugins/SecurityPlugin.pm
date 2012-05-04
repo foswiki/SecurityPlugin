10b6f3681859c38e88eb1bc775465cffb2adc5c9
OlivierRaginel <OlivierRaginel@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2012-05-04 13:27:49 +0000

    Item11808: perltidy them all, so people are not accidentally impacted by the new enforced rule -- Sorry Micha, could not find a way to define specific values within the file
    git-svn-id: http://svn.foswiki.org/trunk/SecurityPlugin@14686 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- lib/TWiki/Plugins/SecurityPlugin.pm
+++ lib/TWiki/Plugins/SecurityPlugin.pm
@@ -11,7 +11,7 @@
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details, published at 
+# GNU General Public License for more details, published at
 # http://www.gnu.org/copyleft/gpl.html
 #
 # =========================
@@ -41,24 +41,24 @@
 #   getSessionValueHandler  ( $key )                                1.010  Use only in one Plugin
 #   setSessionValueHandler  ( $key, $value )                        1.010  Use only in one Plugin
 #
-# initPlugin is required, all other are optional. 
+# initPlugin is required, all other are optional.
 # For increased performance, all handlers except initPlugin are
 # disabled. To enable a handler remove the leading DISABLE_ from
 # the function name. Remove disabled handlers you do not need.
 #
-# NOTE: To interact with TWiki use the official TWiki functions 
+# NOTE: To interact with TWiki use the official TWiki functions
 # in the TWiki::Func module. Do not reference any functions or
 # variables elsewhere in TWiki!!
 
-
 # =========================
-package TWiki::Plugins::SecurityPlugin;    # change the package name and $pluginName!!!
+package TWiki::Plugins::SecurityPlugin
+  ;    # change the package name and $pluginName!!!
 
 # =========================
 use vars qw(
-        $web $topic $user $installWeb $VERSION $RELEASE $pluginName
-        $debug $exampleCfgVar
-    );
+  $web $topic $user $installWeb $VERSION $RELEASE $pluginName
+  $debug $exampleCfgVar
+);
 
 # This should always be $Rev$ so that TWiki can determine the checked-in
 # status of the plugin. It is used by the build automation tools, so
@@ -70,57 +70,59 @@ $VERSION = '$Rev$';
 # of the version number in PLUGINDESCRIPTIONS.
 $RELEASE = 'Dakar';
 
-$pluginName = 'SecurityPlugin';  # Name of this Plugin
+$pluginName = 'SecurityPlugin';    # Name of this Plugin
 
 # =========================
-sub initPlugin
-{
+sub initPlugin {
     ( $topic, $web, $user, $installWeb ) = @_;
 
     # check for Plugins.pm versions
-    if( $TWiki::Plugins::VERSION < 1.000 ) {
-        TWiki::Func::writeWarning( "Version mismatch between $pluginName and Plugins.pm" );
+    if ( $TWiki::Plugins::VERSION < 1.000 ) {
+        TWiki::Func::writeWarning(
+            "Version mismatch between $pluginName and Plugins.pm");
         return 0;
     }
 
     # Get plugin debug flag
-    $debug = TWiki::Func::getPluginPreferencesFlag( "DEBUG" );
+    $debug = TWiki::Func::getPluginPreferencesFlag("DEBUG");
 
-    # Get plugin preferences, the variable defined by:          * Set EXAMPLE = ...
+# Get plugin preferences, the variable defined by:          * Set EXAMPLE = ...
 #    $exampleCfgVar = TWiki::Func::getPluginPreferencesValue( "EXAMPLE" ) || "default";
-    $SecurityLockedCfgVar = TWiki::Func::getPluginPreferencesValue( "SECURITYLOCKED" ) || "LOCKED";
-    $SecurityUnlockedCfgVar = TWiki::Func::getPluginPreferencesValue( "SECURITYUNLOCKED" ) || "UNLOCKED";
-    
+    $SecurityLockedCfgVar =
+      TWiki::Func::getPluginPreferencesValue("SECURITYLOCKED") || "LOCKED";
+    $SecurityUnlockedCfgVar =
+      TWiki::Func::getPluginPreferencesValue("SECURITYUNLOCKED") || "UNLOCKED";
+
     # Plugin correctly initialized
-    TWiki::Func::writeDebug( "- TWiki::Plugins::${pluginName}::initPlugin( $web.$topic ) is OK" ) if $debug;
+    TWiki::Func::writeDebug(
+        "- TWiki::Plugins::${pluginName}::initPlugin( $web.$topic ) is OK")
+      if $debug;
     return 1;
 }
 
 # =========================
-sub handleSecurity
-{
+sub handleSecurity {
 ### my ( $text, $topic, $web ) = @_;   # do not uncomment, use $_[0], $_[1]... instead
 
-    TWiki::Func::writeDebug( "- ${pluginName}::handleSecurity( $_[2].$_[1] )" ) if $debug;
+    TWiki::Func::writeDebug("- ${pluginName}::handleSecurity( $_[2].$_[1] )")
+      if $debug;
 
     # Check if the the page being viewed is authenticated or not
     # and render contents of appropriate output string
-    if (TWiki::Func::isGuest )
-      {
-	  return $SecurityUnlockedCfgVar;
-      }
-    else
-      {
-	  return $SecurityLockedCfgVar;
-      }
+    if (TWiki::Func::isGuest) {
+        return $SecurityUnlockedCfgVar;
+    }
+    else {
+        return $SecurityLockedCfgVar;
+    }
 }
 
 # =========================
-sub commonTagsHandler
-{
+sub commonTagsHandler {
 ### my ( $text, $topic, $web ) = @_;   # do not uncomment, use $_[0], $_[1]... instead
 
-    TWiki::Func::writeDebug( "- ${pluginName}::commonTagsHandler( $_[2].$_[1] )" ) if $debug;
+    TWiki::Func::writeDebug("- ${pluginName}::commonTagsHandler( $_[2].$_[1] )")
+      if $debug;
 
     # This is the place to define customized tags and variables
     # Called by TWiki::handleCommonTags, after %INCLUDE:"..."%
